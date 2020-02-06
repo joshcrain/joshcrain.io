5ea819f627aa542089daefabb0fe99ddebf93e64
Josh Crain <josh@agencybloc.com>
2020-02-06 10:33:28 -0600

    location of service worker matters

--- _site/js/init.js
+++ _site/js/init.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // ServiceWorker is a progressive technology. Ignore unsupported browsers
   if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress.');
-    navigator.serviceWorker.register('/js/service-worker.js').then(function() {
+    navigator.serviceWorker.register('../service-worker.js').then(function() {
       console.log('CLIENT: service worker registration complete.');
     }, function() {
       console.log('CLIENT: service worker registration failure.');
