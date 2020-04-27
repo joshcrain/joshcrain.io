02272a3055cf61edc11a2cbfd5ceb20ccdf4afea
Josh Crain <josh@agencybloc.com>
2020-04-27 05:34:09 -0500

    css min

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,17 @@ module.exports = function(eleventyConfig) {
   });
 };
 
+const Terser = require("terser");
+eleventyConfig.addFilter("jsmin", function(code) {
+    let minified = Terser.minify(code);
+    if( minified.error ) {
+        console.log("Terser error: ", minified.error);
+        return code;
+    }
+
+    return minified.code;
+});
+
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
