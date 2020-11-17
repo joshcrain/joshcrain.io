9d823d6baa4b8d4ea24acc48c7854f2cf094f23e
Joshua Crain <joshuacrain@MacBook-Pro.local>
2020-11-17 14:23:10 -0600

    add clean-css

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
+const CleanCSS = require("clean-css");
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+};
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
