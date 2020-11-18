c0e9d19bc0e5b9db7201157fb4716e01dc6ecd5a
Joshua Crain <joshuacrain@MacBook-Pro.local>
2020-11-18 10:17:58 -0600

    add moment.js and comment changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
 const CleanCSS = require("clean-css");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const moment = require("moment");
+
 module.exports = function(eleventyConfig) {
+  // clean css
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
-};
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-module.exports = function(eleventyConfig) {
+
+  // rss filter
   eleventyConfig.addPlugin(pluginRss);
+
+  // date filter
+  eleventyConfig.addNunjucksFilter("date", function (date, format) {
+  return moment(date).format(format);
+});
 };
