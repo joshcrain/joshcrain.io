2dd39213ef68baa01a0439dc7c364a6c42dd30d9
Joshua Crain <joshuacrain@MacBook-Pro.local>
2020-12-15 10:01:28 -0600

    add lazy images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const CleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const moment = require("moment");
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = function(eleventyConfig) {
   // clean css
@@ -14,5 +15,8 @@ module.exports = function(eleventyConfig) {
   // date filter
   eleventyConfig.addNunjucksFilter("date", function (date, format) {
   return moment(date).format(format);
+  
+  // lazy images
+  eleventyConfig.addPlugin(lazyImagesPlugin);
 });
 };
