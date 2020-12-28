891676e1575a5a5c00ec83882d334e081cceac0b
joshcrain <josh.crain@gmail.com>
2020-12-28 16:37:51 -0600

    initial eleventy images commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const CleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const moment = require("moment");
+const Image = require("@11ty/eleventy-img");
 const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = function(eleventyConfig) {
@@ -15,6 +16,40 @@ module.exports = function(eleventyConfig) {
   // date filter
   eleventyConfig.addNunjucksFilter("date", function (date, format) {
     return moment(date).format(format);
+  });
+
+  // responsive images
+    // works also with addLiquidShortcode or addJavaScriptFunction
+  eleventyConfig.addNunjucksAsyncShortcode("responsiveimage", async function(src, alt, sizes = "100vw") {
+    if(alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on Image from: ${src}`);
+    }
+
+    let metadata = await Image(src, {
+      widths: [300, 600],
+      formats: ['webp', 'jpeg'],
+      outputDir: "./_site/img/",
+    });
+
+    let lowestSrc = metadata["jpeg"][0];
+    let highResJpeg = metadata["jpeg"][1];
+    let lowReswebp = metadata["webp"][0];
+    let highReswebp = metadata["webp"][1];
+
+    const source = `<source type="image/webp" media="(max-width: 629px)" srcset="${lowReswebp.url}" >
+                    <source type="image/webp" media="(min-width: 630px)" srcset="${highReswebp.url}" >
+                    <source type="image/jpeg" media="(max-width: 529px)" srcset="${lowestSrc.url}" >
+                    <source type="image/jpeg" media="(min-width: 630px)" srcset="${highResJpeg.url}" >`;
+
+                    const img = `<img 
+                    loading="lazy" 
+                    alt="${alt}" 
+                    width="${highResJpeg.width}"
+                    height="${highResJpeg.height}"
+                    src="${lowestSrc.url}">`;
+    
+        return `<picture>${source}${img}</picture>`;
   });
 
   // lazy images
---
+++ notes/2020/itll-all-be-over-by-christmas.md
@@ -0,0 +1,7 @@
+---
+pageTitle: I'll all be over by Christmas
+tags: ['notes']
+metaDescription: Forsaken places.
+metaImage: 
+date: 2020-12-24
+---
