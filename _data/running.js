232d0ccf4ea7a7f4c9594c2f03390a065334b093
joshcrain <josh.crain@gmail.com>
2021-11-23 09:52:46 -0600

    incorrect comment add 22

--- _data/running.js
+++ _data/running.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 module.exports = async function() {
     console.log( "Fetching Garmin Connect Data…" );
 
-  // GitHub API: Garmin Connect public activity list JSON feed
+  // Garmin API: Garmin Connect public activity list JSON feed
     return fetch("https://connect.garmin.com/proxy/activitylist-service/activities/8ee2fc52-fe2a-403a-9cc3-5113d520ab51?limit=9999")
         .then(res => res.json()) // node-fetch option to transform to json
         .then(json => {
