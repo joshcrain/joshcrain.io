fbc1a90d606dfcf1e7843a4238cbea20a6a51713
joshcrain <josh.crain@gmail.com>
2023-06-01 16:47:44 -0500

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,24 @@ const CleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const moment = require("moment");
 const Image = require("@11ty/eleventy-img");
+const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addTransform("htmlmin", function(content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+  
   // clean css
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
