118aea8e901cbeecd0875d6a7df30c6a0abff918
joshcrain <josh.crain@gmail.com>
2023-10-11 11:43:26 -0500

    fetch drawings

--- _data/drawing.js
+++ _data/drawing.js
@@ -1,15 +1,20 @@
-const fetch = require("node-fetch");
+const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
-    console.log( "Fetching Drawing Cloudinary Data…" );
-
-  // Cloudinary API
-    return fetch("https://res.cloudinary.com/joshcrain/image/list/drawing.json")
-        .then(res => res.json()) // node-fetch option to transform to json
-        .then(json => {
-        // return the data
-        return {
-            cloudinary: json
-        };
+  try {
+    // Cloudinary drawing feed
+    let json = await EleventyFetch("https://res.cloudinary.com/joshcrain/image/list/drawing.json", {
+      duration: "1d", // 1 day
+      type: "json" // also supports "text" or "buffer"
     });
+    console.log( "Fetching drawing Cloudinary Data…" );
+    return {
+        cloudinary: json
+    };
+  } catch(e) {
+    console.log( "Failed fetching drawing Cloudinary Data…" );
+    return {
+        cloudinary: 0
+    };
+  }
 };
