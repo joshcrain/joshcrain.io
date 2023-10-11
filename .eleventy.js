f29384bb29293e268b7cfea63b2659a57a58ad8c
joshcrain <josh.crain@gmail.com>
2023-10-11 11:43:00 -0500

    add eleveny img

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const CleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const moment = require("moment");
-const Image = require("@11ty/eleventy-img");
 const htmlmin = require("html-minifier");
+
+const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
 
@@ -75,44 +77,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("notes/*/*/*.jpg");
   eleventyConfig.addPassthroughCopy("notes/*/*/*.png");
   eleventyConfig.addPassthroughCopy("notes/*/*/*.gif");
+  
 
-  // responsive images
-    // works also with addLiquidShortcode or addJavaScriptFunction
-  eleventyConfig.addLiquidShortcode("responsiveimage", async function(src, alt, sizes = "100vw") {
-    if(alt === undefined) {
-      // You bet we throw an error on missing alt (alt="" works okay)
-      throw new Error(`Missing \`alt\` on Image from: ${src}`);
-    }
-
-    let metadata = await Image(src, {
-      widths: [218, 438, 686, 750],
-      formats: ['webp', 'jpeg'],
-      outputDir: "./_site/img/",
-    });
-
-    let lowestSrc = metadata["jpeg"][1];
-    let mediumSrc = metadata["jpeg"][2];
-    let highResJpeg = metadata["jpeg"][3];
-    let lowReswebp = metadata["webp"][1];
-    let mediumReswebp = metadata["webp"][2];
-    let highReswebp = metadata["webp"][3];
-
-    const source = `<source type="image/webp" media="(max-width: 480px)" srcset="${lowReswebp.url}" >
-                    <source type="image/webp" media="(max-width: 768px)" srcset="${mediumReswebp.url}" >                
-                    <source type="image/webp" media="(min-width: 769px)" srcset="${highReswebp.url}" >
-                    <source type="image/jpeg" media="(max-width: 480px)" srcset="${lowestSrc.url}" >
-                    <source type="image/jpeg" media="(max-width: 768px)" srcset="${mediumSrc.url}" >
-                    <source type="image/jpeg" media="(min-width: 769px)" srcset="${highResJpeg.url}" >`;
-
-                    const img = `<img 
-                    loading="lazy" 
-                    alt="${alt}" 
-                    width="${highResJpeg.width}"
-                    height="${highResJpeg.height}"
-                    src="${lowestSrc.url}">`;
-    
-                    return `<picture>${source}${img}</picture>`;
-  });
 
   // minify js
   const { minify } = require("terser");
@@ -129,4 +95,25 @@ module.exports = function(eleventyConfig) {
       callback(null, code);
     }
   });
+
+
+
+  eleventyConfig.addShortcode("storyImage", async function(src, alt) {
+		let metadata = await Image(src, {
+			widths: [300, 600],
+			formats: ["avif", "jpeg"],
+      outputDir: "./_site/img/"
+		});
+
+		let imageAttributes = {
+			alt,
+			sizes: "(min-width: 30em) 50vw, 100vw",
+			loading: "lazy",
+			decoding: "async",
+		};
+
+		// You bet we throw an error on a missing alt (alt="" works okay)
+		return Image.generateHTML(metadata, imageAttributes);
+	});
+
 };
