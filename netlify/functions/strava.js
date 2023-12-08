bd8209051dd5b5f3e53b2b47da07b6c87075fc7a
joshcrain <josh.crain@gmail.com>
2023-12-08 10:02:41 -0600

    try again

--- netlify/functions/strava.js
+++ netlify/functions/strava.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
+const fetch = require('@11ty/eleventy-fetch');
 
 exports.handler = async function (event, context) {
   const { access_token } = process.env; // Access token stored as environment variable
   const url = 'https://www.strava.com/api/v3/athlete/activities';
 
   try {
-    const response = await axios.get(url, {
+    const response = await fetch.get(url, {
       headers: { Authorization: `Bearer ${access_token}` },
     });
 
