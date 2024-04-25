94315a6d0ed3b5f428ae56c50d2a01dfd03c9268
joshcrain <josh.crain@gmail.com>
2024-04-24 20:38:38 -0500

    pass through fonts

--- .eleventy.js
+++ .eleventy.js
@@ -66,6 +66,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site.webmanifest");
   eleventyConfig.addPassthroughCopy("google88f9e8b0497a35bd.html");
   eleventyConfig.addPassthroughCopy("*.txt");
+  eleventyConfig.addPassthroughCopy("fonts/");
   
 
   // minify js
